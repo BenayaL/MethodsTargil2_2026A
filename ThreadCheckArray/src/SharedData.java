35609cd807606c264f1c253598686dae52f7d12b
Uriel Julian <160397372+juligantos@users.noreply.github.com>
2025-11-02 10:56:31 +0200

    added some documentation

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
+/**
+ * The following class holds and shares data between 
+ * components or threads
+ */
 public class SharedData 
 {
 	private ArrayList<Integer> array;
@@ -7,37 +11,59 @@ public class SharedData
 	private boolean flag;
 	private final int b;
 	
+	/**
+	 * @param array		Holds list of integer variables
+	 * @param b			Integer that holds the sum of 2 numbers
+	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * @return			getter that sends an array of boolean values
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * @param winArray		setter that updates the boolean array
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**
+	 * @return		getter that sends an array of integer values
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * @return		the final integer b
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * @return		the boolean value of flag
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * @param flag		setter for the boolean parameter flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
